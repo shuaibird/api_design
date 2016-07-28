75ef87c82a31988cb83fbb68fb52fec471ab3c9c
Shuaibird <shuaibird@icloud.com>
2016-07-28 11:33:38 +0800

    api CRUD uint testing

--- server/spec.js
+++ server/spec.js
@@ -1,5 +1,59 @@
+const TEST_DATA = {name: 'Shuabird'}
+
 var app = require('./server')
-var request = require('supertest')
+var supertest = require('supertest')
 var expect = require('chai').expect
 
 
+describe('itemsRouter', () => {
+  it('should get all items', done =>
+    supertest(app)
+      .get('/items')
+      .end((err, res) => {
+        expect(res.body).to.be.an('array')
+        done()
+      })
+  )
+
+  it('should create an item', done =>
+    supertest(app)
+      .post('/items')
+      .send(TEST_DATA)
+      .end((err, res) => {
+        expect(res.body).to.be.an('object')
+        done()
+      })
+  )
+
+  it('should delete an item', done =>
+    supertest(app)
+      .post('/items')
+      .send(TEST_DATA)
+      .end((err, res) => {
+        var item = res.body
+        supertest(app)
+          .delete(`/items/${item.id}`)
+          .end((err, res) => {
+            expect(res.body).to.eql(item)
+            done()
+          })
+      })
+  )
+
+  it('should update an item', done =>
+    supertest(app)
+      .post('/items')
+      .send(TEST_DATA)
+      .end((err, res) => {
+        var item = res.body
+        var updatedItem = {name: item.name.split('')}
+        supertest(app)
+          .put(`/items/${item.id}`)
+          .send(updatedItem)
+          .end((err, res) => {
+            expect(res.body.name).to.eql(updatedItem.name)
+            done()
+          })
+      })
+  )
+})
